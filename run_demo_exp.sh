0fa8763ca1a02721eb63ee7080cba6f9ac2b5a99
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-02 17:13:38 +0800

    com

---
+++ run_demo_exp.sh
@@ -0,0 +1,6 @@
+DEVICES=0,1
+
+CUDA_VISIBLE_DEVICES=${DEVICES} python3 initial_train.py --config yolact_resnet50_pascal_config_init --batch_size 32
+CUDA_VISIBLE_DEVICES=${DEVICES} python3 incremental_train.py --config yolact_resnet50_pascal_config_init --batch_size 32
+
+# CUDA_VISIBLE_DEVICES=${DEVICES} sh ./scripts/
