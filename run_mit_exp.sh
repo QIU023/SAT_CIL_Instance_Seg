1f9fa7ee0c502e5577143f4b0cc046f28bbbfe05
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-10 18:14:37 +0800

    com

--- run_mit_exp.sh
+++ run_mit_exp.sh
@@ -1,6 +1,7 @@
 DEVICES=1,2,6,7
 
-CUDA_VISIBLE_DEVICES=${DEVICES} python3 initial_train.py --config yolact_mitb2_pascal_config_init --batch_size 32 --resume interrupt
-# CUDA_VISIBLE_DEVICES=${DEVICES} python3 incremental_train.py --config yolact_mitb2_pascal_config_init --batch_size 32
+# CUDA_VISIBLE_DEVICES=${DEVICES} python3 initial_train.py --config yolact_mitb2_pascal_config_init --batch_size 32 --resume interrupt
+CUDA_VISIBLE_DEVICES=${DEVICES} python3 expert_train.py --config
+CUDA_VISIBLE_DEVICES=${DEVICES} python3 incremental_train.py --config yolact_mitb2_pascal_config_incremental --batch_size 32 --load_distillation_net /opt/tiger/occupy_arnold/SATIS/weights/1-19/mix_transformer_114_30000.pth
 
 # CUDA_VISIBLE_DEVICES=${DEVICES} sh ./scripts/
