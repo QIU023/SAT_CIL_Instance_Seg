1a37e83d4e65db9da4ae0afafb8436979693537c
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-10 18:14:37 +0800

    com

--- data/config.py
+++ data/config.py
@@ -866,6 +866,28 @@ yolact_mitb2_pascal_config_init = yolact_resnet50_config.copy({
     'lr_steps': (60000, 100000),
 })
 
+yolact_mitb2_pascal_config_expert = yolact_resnet50_config.copy({
+    'name': 'mix_transformer',  # Will default to yolact_resnet50_pascal
+
+    # Dataset stuff
+    'dataset': pascal_sbd_dataset,
+    'num_classes': len(pascal_sbd_dataset.class_names) + 1,
+    'distillation':True,
+    'expert':True,
+    'total_num_classes': 21,
+    'first_num_classes':19,
+    'extend':1,
+    'ratio': 2,
+    'max_iter': 50000,
+    'lr_steps': (30000, 40000),
+
+    'backbone': mit_b2_backbone.copy({
+        'pred_scales': [[16], [32], [64], [128], [256], [512]],
+        'use_square_anchors': False,
+        'path': 'mit_b2.pth',
+    }),
+})
+
 yolact_mitb2_pascal_config_incremental = yolact_resnet50_config.copy({
     'name': 'mix_transformer', # Will default to yolact_resnet50_pascal
     
