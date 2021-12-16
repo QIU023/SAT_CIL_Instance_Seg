6f06793efe080c5f83f8ec78e50236f0de740521
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-16 17:55:05 +0800

    com

--- SAT_scripts/run_mit_19-1_MT.sh
+++ SAT_scripts/run_mit_19-1_MT.sh
@@ -1,7 +1,8 @@
-DEVICES=4
+DEVICES=3
 
 # CUDA_VISIBLE_DEVICES=${DEVICES} python3 initial_train.py --config yolact_mitb2_pascal_config_init --batch_size 8 --resume interrupt
 # CUDA_VISIBLE_DEVICES=${DEVICES} python3 expert_train.py --config yolact_mitb2_pascal_config_expert --batch_size 8 
-CUDA_VISIBLE_DEVICES=${DEVICES} python3 incremental_train.py --config yolact_mitb2_pascal_config_incremental --resume weights/19+1/mix_transformer_22_1171_interrupt_3.pth --batch_size 8 --load_expert_net weights/20/mix_transformer_769_10000.pth --load_distillation_net weights/1-19/mix_transformer_114_30000.pth
+CUDA_VISIBLE_DEVICES=${DEVICES} python3 incremental_train.py --config yolact_mitb2_pascal_config_incremental --resume weights/19-1/19-1_1_student_40497_interrupt.pth --batch_size 8 --load_expert_net weights/19-1/19-1_20expect_network.pth --load_distillation_net weights/19-1/19-1_0_old_teacher.pth
 
 # CUDA_VISIBLE_DEVICES=${DEVICES} sh ./scripts/
+SATIS/weights/19-1/19-1_1_student_40497_interrupt.pth
