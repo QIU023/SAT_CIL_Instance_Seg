46982caf7578860a804fdc4a915201f254997b93
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-18 00:16:15 +0800

    com

--- SAT_scripts/run_mit_10-1_MT.sh
+++ SAT_scripts/run_mit_10-1_MT.sh
@@ -9,5 +9,5 @@ CUDA_VISIBLE_DEVICES=${DEVICES} nohup python3 initial_train.py \
     --config yolact_mitb2_pascal_config_init_10with1 \
     --batch_size 8 \
     --save_folder weights/10-1/ \
-    --resume weights/10-1/10-1_0_student.pth > 'train_log/'${exp_name}'.log' 2>&1 &
+    --resume weights/10-1/mix_transformer_76_47684_interrupt.pth > 'train_log/'${exp_name}'.log' 2>&1 &
 
