710d09cce88d9b54896ce80a1e095b7d855aff75
qiuyiqiao <qiuyiqiao@bytedance.com>
2021-12-18 00:02:49 +0800

    com

--- SAT_scripts/run_mit_10-1_MT.sh
+++ SAT_scripts/run_mit_10-1_MT.sh
@@ -2,5 +2,12 @@ if  [ $# -ge 1 ]
 then
     DEVICES=$1
 fi
-CUDA_VISIBLE_DEVICES=${DEVICES} python3 initial_train.py --config yolact_mitb2_pascal_config_init_10with1 --batch_size 8 --save_folder weights/1-10/
+
+exp_name='10-1_0_student_exp'
+
+CUDA_VISIBLE_DEVICES=${DEVICES} nohup python3 initial_train.py \
+    --config yolact_mitb2_pascal_config_init_10with1 \
+    --batch_size 8 \
+    --save_folder weights/10-1/ \
+    --resume weights/10-1/10-1_0_student.pth > 'train_log/'${exp_name}'.log' 2>&1 &
 
